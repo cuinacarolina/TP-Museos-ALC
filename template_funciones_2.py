1e0f8b032174a54413666930b5578032fff49eb0
Juanagalamoran <165917310+Juanagalamoran@users.noreply.github.com>
2025-06-10 12:26:57 -0300

    anda todo!

--- template_funciones_2.py
+++ template_funciones_2.py
@@ -1,20 +1,20 @@
-#Presentación del grupo
+#PresentaciÃ³n del grupo
 
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Materia: Álgebra Lineal Computacional 
+Materia: Ãlgebra Lineal Computacional 
 Trabajo Practico 2
 Equipo: Brasil
-Autores: Carolina Julia Cuina, Juana Gala Moran, María Juliana Salfity
+Autores: Carolina Julia Cuina, Juana Gala Moran, MarÃ­a Juliana Salfity
 """
 #%%
 import numpy as np
 from scipy.linalg import solve_triangular
 import matplotlib.pyplot as plt
 import pandas as pd # Para leer archivos
-import geopandas as gpd # Para hacer cosas geográficas
-import networkx as nx # Construcción de la red en NetworkX
+import geopandas as gpd # Para hacer cosas geogrÃ¡ficas
+import networkx as nx # ConstrucciÃ³n de la red en NetworkX
 from scipy.linalg import solve_triangular
 import template_funciones as tp1
 # Matriz A de ejemplo
@@ -29,23 +29,22 @@ A_ejemplo = np.array([
     [0, 0, 0, 0, 1, 1, 1, 0]
 ])
 #%%
-# Leemos el archivo, retenemos aquellos museos que están en CABA, y descartamos aquellos que no tienen latitud y longitud
+# Leemos el archivo, retenemos aquellos museos que estÃ¡n en CABA, y descartamos aquellos que no tienen latitud y longitud
 museos = gpd.read_file('https://raw.githubusercontent.com/MuseosAbiertos/Leaflet-museums-OpenStreetMap/refs/heads/principal/data/export.geojson')
 barrios = gpd.read_file('https://cdn.buenosaires.gob.ar/datosabiertos/datasets/ministerio-de-educacion/barrios/barrios.geojson')
-# En esta línea:
-# Tomamos museos, lo convertimos al sistema de coordenadas de interés, extraemos su geometría (los puntos del mapa),
+# En esta lÃ­nea:
+# Tomamos museos, lo convertimos al sistema de coordenadas de interÃ©s, extraemos su geometrÃ­a (los puntos del mapa),
 # calculamos sus distancias a los otros puntos de df, redondeamos (obteniendo distancia en metros), y lo convertimos a un array 2D de numpy
 D = museos.to_crs("EPSG:22184").geometry.apply(lambda g: museos.to_crs("EPSG:22184").distance(g)).round().to_numpy()
 # Leemos el archivo vector w
-w = pd.read_csv("visitas.txt", sep="\t", header=None).values.flatten()
+#w = pd.read_csv("visitas.txt", sep="\t", header=None).values.flatten()
 
-A = tp1.construye_adyacencia(D, 3)
-print(A)
+
 #%%
 def calcula_L(A):
     K = tp1.calcula_matriz_K(A)
     return K - A
-L = calcula_L(A_ejemplo)
+
 
 #%%
 def calcula_P(A):
@@ -57,7 +56,6 @@ def calcula_P(A):
     for i in range(n):
         for j in range(n):
             P[i][j] = (grados[i] * grados[j]) / conexiones
-    
     return P
 
 P = calcula_P(A_ejemplo)
@@ -93,46 +91,53 @@ def calcula_Q(R,v):
 # Calculamos autovalores y autovectores
 valores, vectores = np.linalg.eig(A_ejemplo)
 
-# Tomamos el autovalor más grande y su correspondiente autovector
+# Tomamos el autovalor mÃ¡s grande y su correspondiente autovector
 indice_max = np.argmax(valores)
 autovalor_principal = valores[indice_max]
 autovector_principal = vectores[:, indice_max]
 
-lambda_ejemplo = calcula_lambda(L, autovector_principal)
+#lambda_ejemplo = calcula_lambda(L, autovector_principal)
 
 q_ejemplo = calcula_Q(R, autovector_principal)
 #%%
-A = tp1.construye_adyacencia(D, 50)
+
 
 def metpot1(A,tol=1e-8,maxrep=np.inf):
    # Recibe una matriz A y calcula su autovalor de mayor módulo, con un error relativo menor a tol y-o haciendo como mucho maxrep repeticiones
-   np.random.seed(1)
-   v = np.random.uniform(-1, 1, A.shape[0]) # Generamos un vector de partida aleatorio, entre -1 y 1
-   v = v = v / np.linalg.norm(v) # Lo normalizamos
-   v1 = A @ v # Aplicamos la matriz una vez
-   #print(np.linalg.norm(v1))
-   v1 = v1 / np.linalg.norm(v1) # normalizamos
-   l = (v.T @ (A @ v)) / (v.T @ v) # Calculamos el autovalor estimado
-   l1 = (v1.T @ (A @ v1)) / (v1.T @ v1) # Y el estimado en el siguiente paso
-   nrep = 0 # Contador
-   while np.abs(l1-l)/np.abs(l) > tol and nrep < maxrep: # Si estamos por debajo de la tolerancia buscada 
-      v = v1 # actualizamos v y repetimos
-      l = l1
-      v1 = A @ v # Calculo nuevo v1
-      v1 = v1 / np.linalg.norm(v1) # Normalizo
-      l1 = (v1.T @ (A @ v1)) / (v1.T @ v1) # Calculo autovalor
-      nrep += 1 # Un pasito mas
+   n,_ = A.shape
+   v = np.random.rand(n) # Generamos un vector de partida aleatorio, entre -1 y 1
+   v = v/ np.linalg.norm(v) # Lo normalizamos
+
+   v1 = A @ v  #Aplicamos la matriz una vez
+   v1 = (v1)/np.linalg.norm(v1) #normalizamos
+   l = (v.T @ A @ v) / (v.T @ v) #Calculamos el autovalor estimado
+   nrep = 0 #Contador
+
+   l1 = (v1.T @ A @ v1) / (v1.T @ v1) # Y el estimado en el siguiente paso   
+
+   while np.abs(l1 - l) / np.abs(l) > tol and nrep < maxrep:
+        norm_v1 = np.linalg.norm(v1)
+        if norm_v1 < 1e-12:
+            print("Norma casi cero: v1 =", v1)
+            return v1, 0, False
+        v = v1.copy()
+        l = l1
+        v1 = A @ v1
+        v1 = v1 / np.linalg.norm(v1)
+        l1 = (v1.T @ A @ v1) / (v1.T @ v1)
+
+        nrep += 1
+
    if not nrep < maxrep:
       print('MaxRep alcanzado')
-   l = (v.T @ (A @ v)) / (v.T @ v) # Calculamos el autovalor
    return v1,l,nrep<maxrep
 
-metpot1(A)
-#autovalor_max_ejemplo = metpot1(A_ejemplo)
+
+print(metpot1(A_ejemplo))
 #%%
 def deflaciona(A,tol=1e-8,maxrep=np.inf):
-    # Recibe la matriz A, una tolerancia para el método de la potencia, y un número máximo de repeticiones
-    v1,l1, _ = metpot1(A,tol,maxrep) # Buscamos primer autovector con método de la potencia
+    # Recibe la matriz A, una tolerancia para el mÃ©todo de la potencia, y un nÃºmero mÃ¡ximo de repeticiones
+    v1,l1, _ = metpot1(A,tol,maxrep) # Buscamos primer autovector con mÃ©todo de la potencia
     deflA = A - l1 * np.outer(v1, v1) # Sugerencia, usar la funcion outer de numpy
     return deflA
 
@@ -149,8 +154,9 @@ def metpot2(A,v1,l1,tol=1e-8,maxrep=np.inf):
 #segundo_autovalor_ejemplo = metpot2(A_ejemplo, v1, l1)
 #%%
 def metpotI(A,mu,tol=1e-8,maxrep=np.inf):
-    # Retorna el primer autovalor de la inversa de A + mu * I, junto a su autovector y si el método convergió.
-    #calculo el autovalor la inversa de A + mu * I
+    # Retorna el primer autovalor de la inversa de A + mu * I, junto a su autovector y si el mÃ©todo convergiÃ³.
+    #calculo el    v = V[:,0]
+    #autovalor la inversa de A + mu * I
     n,_ = A.shape #tomo la dimension de A
     I = np.eye(n) #creo la matriz identidad de dimension n
     X = A + mu* I # Calculamos la matriz A shifteada en mu (plantilla)
@@ -160,7 +166,7 @@ def metpotI(A,mu,tol=1e-8,maxrep=np.inf):
 def metpotI2(A,mu,tol=1e-8,maxrep=np.inf):
    # Recibe la matriz A, y un valor mu y retorna el segundo autovalor y autovector de la matriz A, 
    # suponiendo que sus autovalores son positivos excepto por el menor que es igual a 0
-   # Retorna el segundo autovector, su autovalor, y si el metodo llegó a converger.
+   # Retorna el segundo autovector, su autovalor, y si el metodo llegÃ³ a converger.
    n,_ = A.shape #tomo la dimension de A
    I = np.eye(n) #creo la matriz identidad de dimension n
    X = A + mu* I # Calculamos la matriz A shifteada en mu (plantilla)
@@ -174,17 +180,17 @@ def metpotI2(A,mu,tol=1e-8,maxrep=np.inf):
 def laplaciano_iterativo(A,niveles,nombres_s=None):
     # Recibe una matriz A, una cantidad de niveles sobre los que hacer cortes, y los nombres de los nodos
     # Retorna una lista con conjuntos de nodos representando las comunidades.
-    # La función debe, recursivamente, ir realizando cortes y reduciendo en 1 el número de niveles hasta llegar a 0 y retornar.
+    # La funciÃ³n debe, recursivamente, ir realizando cortes y reduciendo en 1 el nÃºmero de niveles hasta llegar a 0 y retornar.
     if nombres_s is None: # Si no se proveyeron nombres, los asignamos poniendo del 0 al N-1
         nombres_s = range(A.shape[0])
-    if A.shape[0] == 1 or niveles == 0: # Si llegamos al último paso, retornamos los nombres en una lista
+    if A.shape[0] == 1 or niveles == 0: # Si llegamos al Ãºltimo paso, retornamos los nombres en una lista
         return([nombres_s])
     else: # Sino:
         L = calcula_L(A) # Recalculamos el L
         v1,l1,_ = metpot1(A,tol=1e-8,maxrep=np.inf)
         v,_,_ = metpot2(L,v1,l1)# Encontramos el segundo autovector de L
         
-        # Separamos los nodos en dos grupos según el signo del segundo autovector
+        # Separamos los nodos en dos grupos segÃºn el signo del segundo autovector
         ind_p = []
         ind_m = []
         for i in range(len(v)):
@@ -221,22 +227,25 @@ def modularidad_iterativo(A=None, R=None, nombres_s=None):
 
     # Autovector principal de R
     v, l, _ = metpot1(R)
-
-    # División según signo del autovector
+    L, V = np.linalg.eig(R)
+    v = V[:,0]
+    print(f"{np.linalg.norm(R @ v - l * v)}")
+    # DivisiÃ³n segÃºn signo del autovector
     ind_p = [i for i in range(len(v)) if v[i] >= 0]
     ind_m = [i for i in range(len(v)) if v[i] < 0]
-
+    print(ind_m)
     # Si no se puede dividir, devolver comunidad entera
     if len(ind_p) == 0 or len(ind_m) == 0:
+        print("no dividio")
         return [nombres_s]
 
-    # Modularidad ganada por esta división
+    # Modularidad ganada por esta divisiÃ³n
     Q = np.sum(R[np.ix_(ind_p, ind_p)]) + np.sum(R[np.ix_(ind_m, ind_m)])
 
     if Q <= 0:
         return [nombres_s]
 
-    # División recursiva
+    # DivisiÃ³n recursiva
     nombres_p = [nombres_s[i] for i in ind_p]
     nombres_m = [nombres_s[i] for i in ind_m]
     Rp = R[np.ix_(ind_p, ind_p)]
@@ -245,79 +254,85 @@ def modularidad_iterativo(A=None, R=None, nombres_s=None):
     return modularidad_iterativo(R=Rp, nombres_s=nombres_p) + \
            modularidad_iterativo(R=Rm, nombres_s=nombres_m)
 
-
-#%% pruebaç
+#modularidad_iterativo(A_ejemplo)
+modularidad_iterativo(tp1.construye_adyacencia(D, 3))
+#%% pruebaÃ§
 
 A = tp1.construye_adyacencia(D, 3)
 comunidades = modularidad_iterativo(A)
-print(len(comunidades))  # ¿Da más de 1?
+print(len(comunidades))  # Â¿Da mÃ¡s de 1?
 print([len(c) for c in comunidades])
 print("Suma de modularidad esperada:", np.sum(R))
-print("Matriz R simétrica:", np.allclose(R, R.T))
+print("Matriz R simÃ©trica:", np.allclose(R, R.T))
 #%%
-lista_m = [3,5,10,50]
-cantidad_de_grupos = modularidad_iterativo(A_ejemplo)
-niveles = cantidad_de_grupos
 
 #primero hacemos la funcion celling:
 def simetriza_y_ceiling(A):
-    print(f"\a = {A}")
     A_sym = 0.5 * (A + A.T)         
     A_moño = np.ceil(A_sym)         
     return A_moño
 
 #Calcula y grafica las comunidades detectadas para distintos valores de m.
-#Parámetros:
+#ParÃ¡metros:
 #  -D: matriz de distancias entre nodos
 #  -lista_m: lista con valores de m para construir A
 #  -metodo: 0 para Laplaciano, 1 para Modularidad
-#  -niveles: cantidad de cortes para el método Laplaciano
+#  -niveles: cantidad de cortes para el mÃ©todo Laplaciano
   
-def comunidades_subplot(D, lista_m, metodo, niveles=5):
+def comunidades_subplot(D, lista_m, metodo, niveles=4):
     if metodo == 0:
         nombre_metodo = "Laplaciano" 
     else:
         nombre_metodo = "Modularidad"
 
-    fig, axes = plt.subplots(1, len(lista_m), figsize=(5 * len(lista_m), 7))
+    fig, axes = plt.subplots(1, len(lista_m), figsize=(7 * len(lista_m), 7))
     if len(lista_m) == 1:
         axes = [axes]  # por si hay un solo subplot
 
+    # Precalculo coordenadas para museos
+    museos['x'] = museos.geometry.x 
+    museos['y'] = museos.geometry.y
+    posiciones = {idx: (row['x'], row['y']) for idx, row in museos.iterrows()}
+
     for i, m in enumerate(lista_m):
-        print(f"\n--- m = {m} ---")
         A = tp1.construye_adyacencia(D, m)
         A_moño = simetriza_y_ceiling(A)
-        print(A_moño)
-        print(A)
 
         if metodo == 0:
             comunidades_detectadas = laplaciano_iterativo(A_moño, niveles=niveles)
+            
         else:
             comunidades_detectadas = modularidad_iterativo(A_moño)
 
         ax = axes[i]
-        barrios.to_crs("EPSG:22184").boundary.plot(ax=ax, color='gray', linewidth=0.5)
-        #extraemos las coordenadas del museo
-        museos['x'] = museos.geometry.x 
-        museos['y'] = museos.geometry.y
-        posiciones = {i: (museos.loc[i, "x"], museos.loc[i, "y"]) for i in range(len(A))}
+        print(axes.shape)
+        barrios.to_crs("EPSG:22184").boundary.plot(ax=ax, color='lightgray', linewidth=0.7)
         G = nx.from_numpy_array(A)
         colores = plt.get_cmap("tab20", len(comunidades_detectadas))
+        
+        print(f"/comunidades={comunidades_detectadas}")
 
         for j, grupo in enumerate(comunidades_detectadas):
-            nx.draw_networkx_nodes(G, posiciones,nodelist=grupo,node_color=[colores(j)],
-                                   node_size=77,
-                                   ax=ax)
-                                   
-        nx.draw_networkx_edges(G, posiciones, ax=ax, alpha=0.5, width=0.5)
-        ax.set_title(f"{nombre_metodo} m={m}")
-        
+            nx.draw_networkx_nodes(
+                G, posiciones,
+                nodelist=grupo,
+                node_color=[colores(j)],
+                node_size=75,
+                ax=ax
+            )
 
-    plt.suptitle(f"Comunidades detectadas con método de {nombre_metodo}", fontsize=17, y=1)
-    plt.tight_layout()
+        nx.draw_networkx_edges(G, posiciones, ax=ax, alpha=0.3, width=0.3)
+        ax.set_title(f"{nombre_metodo} m={m}, comunidades detectadas: {len(comunidades_detectadas)}", fontsize=11)
+
+        # Ajuste de límites geográficos
+        xs, ys = zip(*posiciones.values())
+        ax.set_xlim(min(xs) - 0.01, max(xs) + 0.01)
+        ax.set_ylim(min(ys) - 0.01, max(ys) + 0.01)
+
+    plt.suptitle(f"Comunidades detectadas con método de {nombre_metodo}", fontsize=16)
+    plt.tight_layout(rect=[0, 0, 1, 0.96])  # deja espacio para el título
     plt.show()
 
 
-
-comunidades_subplot(D, [50], metodo=0)
-#comunidades_subplot(D, [3,5,10,50], metodo=1)
+#comunidades_subplot(D, [3,5,10,50], 0)
+comunidades_subplot(D, [3,5,10,50], 1)
